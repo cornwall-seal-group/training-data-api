b26e0c9c1ccffeb3581b3b2e68af7330a46cf14c
Richard Frost <richard.frost@headforwards.com>
2019-05-29 13:27:58 +0100

    Added img processing and saving to filesystem

--- image/crop.py
+++ image/crop.py
@@ -1,7 +1,5 @@
-from PIL import Image
-
 
 def crop_image(img, prediction):
     b_box = prediction.bounding_box
     # left, upper, right, lower
-    return Image.crop(b_box)
+    return img.crop(b_box)
