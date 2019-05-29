efa46f1fd44ec6a52ad29e5e68a8d49fdce404a9
Rich Frost <richard@ipponsolutions.co.uk>
2019-05-29 14:39:23 +0100

    Added storage to JSON file per seal folder

--- image_meta/store.py
+++ image_meta/store.py
@@ -5,13 +5,12 @@ def store_seal_img_metadata(folder, id):
 
     json_file = 'metadata.json'
     file_path = folder + '/' + json_file
-    with open(file_path, 'w') as json_file:
+    with open(file_path, 'w+') as json_file:
         try:
             imgs = json.load(json_file)
         except ValueError:
             imgs = []
 
-        json.dump([], json_file)
         imgs.append({
             "id": id
         })
